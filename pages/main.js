e48aea1b064d5c3b02fea69f790b648f56994455
Ferry Manders <ferrymanders@users.noreply.github.com>
2017-10-05 09:52:11 +0200

    nieuwe versie website

--- pages/main.js
+++ pages/main.js
@@ -1,3 +1,5 @@
+var hostdata = [];
+
 function dump(obj) {
 	var out = '';
 	for (var i in obj) {
@@ -19,18 +21,54 @@ function getVersions(){
 		dataType: "json",
 		success: function(data) {
 			$.each(data, function(key, value) {
-       				var div = document.getElementById(key);
+       	var div = document.getElementById(key);
 				array[key] = value;
 				if (!div)
 				{
-					$("#data").append('<tr id="' + key + '"><td>' + key + '</td><td>' + value + '</td></tr>'); 
+					//$("#data").append('<tr id="' + key + '"><td>' + key + '</td><td>' + value + '</td></tr>'); 
 				}else{
-					$('#'.key).html('<td>' + key + '</td><td>' + value + '</td>');
+					//$('#'.key).html('<td>' + key + '</td><td>' + value + '</td>');
 				}
+				setHostData(key, value);
 			});
 		}
 	});
   
+}
+
+function setHostData(host, value)
+{
+	var array = [];
+	var timenow = (new Date).getTime();
+	array.value = value;
+	array.time = timenow;
+	hostdata[host] = array;
+	for (var prop in hostdata) {
+		if (hostdata.hasOwnProperty(prop)) { 
+			var timecheck = 0;
+			var timenow = (new Date).getTime();
+			timecheck = timenow - hostdata[prop].time;
+			//console.log(' timecheck :' + timecheck);
+			if ( timecheck > 10000 ) {
+				delete hostdata[prop];
+			}
+		}
+	}
+
+	$("#data").empty();
+	for (var prop in hostdata) {
+		if (hostdata.hasOwnProperty(prop)) { 
+				var div = document.getElementById(prop);
+				if (!div)
+				{
+					$("#data").append('<tr id="' + prop + '"><td>' + prop + '</td><td>' + value + '</td></tr>'); 
+				}else{
+					$('#'.prop).html('<td>' + prop + '</td><td>' + value + '</td>');
+				}
+		}
+	}
+	
+	
 }
 
 $(document).ready(function() {
@@ -39,5 +77,6 @@ $(document).ready(function() {
 
 function doActions() {
 	getVersions();
-	setTimeout("doActions()", 1000);
+	//dump(hostdata);
+	setTimeout("doActions()", 500);
 }
